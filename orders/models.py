7b8ffcd1584e6562ec90f2eac1324b37b81aa818
mdShakilHossainNsu20!8 <shakilnsu2018@gmail.com>
2020-06-09 22:40:51 +0600

    Order model done

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from carts.models import Cart
+
+ORDER_STATUS_CHOICES = (
+    ('created', 'Created'),
+    ('paid', 'Paid'),
+    ('shipped', 'Shipped'),
+    ('refunded', 'Refunded'),
+)
+
+
+class Order(models.Model):
+    order_id = models.CharField(max_length=120, blank=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    status = models.CharField(max_length=120, choices=ORDER_STATUS_CHOICES, default='created')
+    shipping_total = models.DecimalField(decimal_places=2, max_digits=100, default=5.00)
+    total = models.DecimalField(decimal_places=2, max_digits=100, default=0.00)
+
+    def __str__(self):
+        return self.order_id
